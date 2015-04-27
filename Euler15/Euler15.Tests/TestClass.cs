fed74c666ba9133561b93aba3ab85bc77f0310d3
michaldz121 <michaldz121@gmail.com>
2015-04-27 22:13:40 +0200

    Dodanie solucji z programem obliczeniowym oraz programem testującym.

---
+++ Euler15/Euler15.Tests/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Euler15.Tests
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            
+        }
+    }
+}
