102ef60dc72b5439b5aa1cecd963112b99c8f803
michaldz121 <michaldz121@gmail.com>
2015-04-27 22:40:24 +0200

    Dodanie testu dla metody SumOfDigitsPower.
    Metoda nie przechodzi testów.

--- Euler15/Euler15.Tests/SumOfPowersTests.cs
+++ Euler15/Euler15.Tests/SumOfPowersTests.cs
@@ -3,9 +3,15 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Euler15.Tests
 {
+    /// <summary>
+    /// Tests for SumOfFifthPowersTests Class
+    /// </summary>
     [TestClass]
     public class SumOfPowersTests
     {
+        /// <summary>
+        /// Tests for GetPower method
+        /// </summary>
         [TestMethod]
         public void GetPowerTest()
         {
@@ -30,6 +36,38 @@ namespace Euler15.Tests
             Assert.AreEqual(expectedValue7_5, actualValue7_5, "Error with base 7 and exponent 5");
             Assert.AreEqual(expectedValue6_7, actualValue6_7, "Error with base 6 and exponent 7");
             Assert.AreEqual(expectedValue4_9, actualValue4_9, "Error with base 4 and exponent 9");
+        }
+
+        /// <summary>
+        /// Tests for SumOfDigitsPower method
+        /// </summary>
+        [TestMethod]
+        public void SumOfDigitsPowerTest()
+        {
+            // Expected values
+            int expectedValue1634_4 = 1634;
+            int expectedValue8208_4 = 8208;
+            int expectedValue9474_4 = 9474;
+            int expectedValue13192_5 = 13192;
+            int expectedValue92727_5 = 92727;
+            int expectedValue194979_5 = 194979;
+
+            // Actual values
+            int actualValue1634_4 = SumOfPowers.SumOfDigitsPower(1634, 4);
+            int actualValue8208_4 = SumOfPowers.SumOfDigitsPower(8208, 4);
+            int actualValue9474_4 = SumOfPowers.SumOfDigitsPower(9474, 4);
+            int actualValue13192_5 = SumOfPowers.SumOfDigitsPower(34564, 5);
+            int actualValue92727_5 = SumOfPowers.SumOfDigitsPower(92727, 5);
+            int actualValue194979_5 = SumOfPowers.SumOfDigitsPower(194979, 5);
+
+
+            // Asserts
+            Assert.AreEqual(expectedValue1634_4, actualValue1634_4, "Error with number 1634 and power 4");
+            Assert.AreEqual(expectedValue8208_4, actualValue8208_4, "Error with number 8208 and power 4");
+            Assert.AreEqual(expectedValue9474_4, actualValue9474_4, "Error with number 9474 and power 4");
+            Assert.AreEqual(expectedValue13192_5, actualValue13192_5, "Error with number 34564 and power 5");
+            Assert.AreEqual(expectedValue92727_5, actualValue92727_5, "Error with number 92727 and power 5");
+            Assert.AreEqual(expectedValue194979_5, actualValue194979_5, "Error with number 194979 and power 5");
         }
     }
 }
--- Euler15/Euler15/SumOfPowers.cs
+++ Euler15/Euler15/SumOfPowers.cs
@@ -43,5 +43,27 @@ namespace Euler15
 
             return baseNumber;
         }
+
+        /// <summary>
+        /// Computes the sum of each digit power in number
+        /// <params name="number">base number</params>
+        /// <params name="digitCount">Count of digits in number</params>
+        /// <returns>Sum of digit power</returns>
+        /// <usecase>int s = SumOfDigitsPower(123, 3)</usecase>
+        /// </summary>
+        public static int SumOfDigitsPower(int number, int power)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Computes the sum of all the numbers that can be written as the sum of fifth powers of their digits
+        /// <params name="power">power</params>
+        /// <returns>Sum of numbers</returns>
+        /// </summary>
+        public int Compute(int power)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
